d7f688484cc8e7691214a4057092a26f73503219
Elikworld <koladeodutoye9913@gmail.com>
2020-06-11 00:49:57 +0100

    ft:refactored and modified

--- app.js
+++ app.js
@@ -1,37 +1,22 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 require("dotenv").config();
-var indexRouter = require("./routes/index");
-var apiRouter = require("./routes/api");
-var apiResponse = require("./helpers/apiResponse");
-var cors = require("cors");
+const indexRouter = require("./routes/index");
+const smsRouter = require("./routes/sms");
+const apiResponse = require("./helpers/apiResponse");
+const mongoDbConnection =require("./configs/dbconfig");
+const cors = require("cors");
 
-// DB connection
-var MONGODB_URL = process.env.MONGODB_URL;
-var mongoose = require("mongoose");
-mongoose
-  .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    //don't show the log when it is test
-    if (process.env.NODE_ENV !== "test") {
-      console.log("Connected to %s", MONGODB_URL);
-      console.log(`App is running on port ${process.env.PORT}... \n`);
-      console.log("Press CTRL + C to stop the process. \n");
-    }
-  })
-  .catch((err) => {
-    console.error("App starting error:", err.message);
-    process.exit(1);
-  });
-var db = mongoose.connection;
+//load the database
+mongoDbConnection();
 
-var app = express();
+const app = express();
 
 //don't show the log when it is test
 if (process.env.NODE_ENV !== "test") {
-  app.use(logger("dev"));
+	app.use(logger("dev"));
 }
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -42,18 +27,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 //Route Prefixes
-app.use("/", indexRouter);
-app.use("/api/", apiRouter);
+app.use("/api/v1", indexRouter);
+app.use("/api/v1/sms", smsRouter);
 
 // throw 404 if URL not found
+
 app.all("*", function (req, res) {
-  return apiResponse.notFoundResponse(res, "Page not found");
-});
+	return apiResponse.notFoundResponse(res, "Page not found");
+});  
 
 app.use((err, req, res) => {
-  if (err.name == "UnauthorizedError") {
-    return apiResponse.unauthorizedResponse(res, err.message);
-  }
+	if (err.name == "UnauthorizedError") {
+		return apiResponse.unauthorizedResponse(res, err.message);
+	}
 });
 
 module.exports = app;
