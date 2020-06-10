54292ded060e90c47cc3db1a96c47943963af3fb
Tobi Ajibade <herityjohnny14>
2020-06-10 11:18:34 -0700

    Fix the twillo phone number

--- controllers/SmsController.js
+++ controllers/SmsController.js
@@ -1,12 +1,18 @@
+
+
 const SmsModel = require("../models/SmsModel");
 const apiResponse = require("../helpers/apiResponse");
 const auth = require("../middlewares/jwt");
 var mongoose = require("mongoose");
 mongoose.set("useFindAndModify", false);
 
+
+//TWILIO CONFIGURATION 
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
+
 
 /**
  * Retrieve Account Balance.
@@ -36,7 +42,7 @@ exports.sendSms = [
       //Setup Twilo and send message
       client.messages.create({
         body: 'This is a test text',
-        from: process.env.TWILIO_ACCOUNT_SID,
+        from: process.env.TWILIO_PHONE_NUMBER,
         to: request.params.phone
       })
        .then(message => console.log(message.sid));
