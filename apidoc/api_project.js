fa05ee324e594ec41cf847c0b789325fff9055db
AironDev <airondev@gmail.com>
2020-06-11 23:58:05 +0100

    created docs for api, copied it into public folder to serve as the landing page

---
+++ apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "SMS Microservice API",
+  "version": "0.1.0",
+  "description": "A simple micro service for sending SMS notifications by #team-titan@HNGi7",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-06-11T22:33:57.611Z",
+    "url": "http://apidocjs.com",
+    "version": "0.23.0"
+  }
+});
